03d7328d072a8c5e09ec576e77e2e5bddfabbab2
YorBros <jordy.bouwmeester@student.hu.nl>
2023-09-17 14:09:47 +0200

    p2

--- src/main/java/nl/hu/dp/Main.java
+++ src/main/java/nl/hu/dp/Main.java
@@ -5,15 +5,60 @@ import nl.hu.dp.Domain.ReizigerDAOPsql;
 
 import java.sql.SQLException;
 import java.sql.*;
+import java.util.List;
 
 public class Main {
     private static Connection connection;
 
     public static void main(String[] args) throws SQLException {
         ReizigerDAOPsql rdao = new ReizigerDAOPsql(getConnection());
-        Reiziger reiziger = new Reiziger(6, "J", "", "Jordy", Date.valueOf("1999-09-09"));
-        rdao.findById(6);
+        testReizigerDAO(rdao);
+//        Reiziger reiziger = new Reiziger(6, "J", "", "Jordy", Date.valueOf("1999-09-09"));
+//        System.out.println(rdao.findByGbdatum("2002-12-03"));
         closeConnection();
+    }
+
+    /**
+     * P2. Reiziger DAO: persistentie van een klasse
+     *
+     * Deze methode test de CRUD-functionaliteit van de Reiziger DAO
+     *
+     * @throws SQLException
+     */
+    private static void testReizigerDAO(ReizigerDAOPsql rdao) throws SQLException {
+        System.out.println("\n---------- Test ReizigerDAO -------------");
+
+        // Haal alle reizigers op uit de database
+        List<Reiziger> reizigers = rdao.findAll();
+        System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+        for (Reiziger r : reizigers) {
+            System.out.println(r);
+        }
+        System.out.println();
+
+        // Maak een nieuwe reiziger aan en persisteer deze in de database
+        String gbdatum = "1981-03-14";
+        Reiziger sietske = new Reiziger(77, "S", "", "Boers", java.sql.Date.valueOf(gbdatum));
+        System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
+        rdao.save(sietske);
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+        System.out.print("[Test] Eerst " + rdao.findById(77) + " reizigers, na ReizigerDAO.update() ");
+        sietske.setAchternaam("Melk");
+        rdao.update(sietske);
+        System.out.println(rdao.findById(77) + "\n");
+
+        System.out.println("[Test] ReizigerDAO.findByGbdatum() geeft de volgende reizigers:");
+        System.out.println(rdao.findByGbdatum("1981-03-14") + "\n");
+
+        // Voeg aanvullende tests van de ontbrekende CRUD-operaties in.
+        System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.delete() ");
+        rdao.delete(sietske);
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+
     }
 
     private static Connection getConnection() throws SQLException {
